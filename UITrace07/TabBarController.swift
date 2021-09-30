b3cad95d150821cd61132b4dcc9ef5daef8a1c31
Nanami Kurata <krtnnm20011031@gmail.com>
2021-09-30 14:42:31 +0900

    first commit

---
+++ UITrace07/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  UITrace07
+//
+//  Created by 倉田七海 on 2021/09/30.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UIViewController {
+    
+    
+}
---
+++ UITrace07/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UITrace07
+//
+//  Created by 倉田七海 on 2021/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var homeLabel: UILabel!
+    @IBOutlet weak var workLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var issueButton: UIButton!
+    @IBOutlet weak var pullrequestButton: UIButton!
+    @IBOutlet weak var repositoriesButton: UIButton!
+    @IBOutlet weak var organizationsButton: UIButton!
+    @IBOutlet weak var favoritesLabel: UILabel!
+    @IBOutlet weak var expLabel: UILabel!
+    @IBOutlet weak var favoritesButton: UIButton!
+    
+}
+
